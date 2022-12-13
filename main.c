ce2ca7eb8c044e85ff9015942a7a8812eb2620c5
LuongVanVu <vuluong1337@gmail.com>
2022-12-13 23:58:33 +0700

    add B1 file

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+extern int tong();
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    printf("%d", tong(2,3));
+    return 0;
+}
---
+++ tinhtong.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int tong(int x, int y)
+{
+    return x + y;
+}
